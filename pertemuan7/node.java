193fda0e5b9358e15d94d324d45f7f86a1154f6e
AlRizzaAPT <136303419+AlRizzaAPT@users.noreply.github.com>
2024-05-19 19:39:23 +0700

    Add files via upload

---
+++ pertemuan7/node.java
@@ -0,0 +1,31 @@
+package pertemuan7;
+
+public class node {
+
+	    private Matakuliah data;
+	    private node next;
+
+
+	    public node(Matakuliah data) {
+	        this.data = data;
+	    }
+
+	    // Setter dan Getter
+
+	    public void setNext(node next) {
+	        this.next = next;
+	    }
+
+	    public Matakuliah getdata() {
+	        return data;
+	    }
+
+	    public node getNext() {
+	        return next;
+	    }
+
+	    public void setdata(Matakuliah data) {
+	        this.data = data;
+	    }
+
+	}
